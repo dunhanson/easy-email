e885d034a84b3dc81653059d7fa641ee16fcab12
dunhanson <dunhanson@gmail.com>
2020-04-25 21:50:08 +0800

    log4j.properties & Start class & update README.md

--- src/test/java/Start.java
+++ src/test/java/Start.java
@@ -1,13 +1,7 @@
-# Easy Email
+import org.junit.Test;
+import site.dunhanson.email.entity.*;
+import site.dunhanson.email.untils.EmailUtils;
 
-简化Apache Email操作
-
-采取非过度封装策略，贴切原有Apache Email的SDK，即方便上手即用
-
-
-## 简单开始
-
-```java
 public class Start {
 
     @Test
@@ -79,73 +73,3 @@ public class Start {
     }
 
 }
-```
-
-
-
-## 配置文件
-
-log4j.properties
-```properties
-### 设置###
-log4j.rootLogger = info, stdout
-
-### 输出信息到控制抬 ###
-log4j.appender.stdout = org.apache.log4j.ConsoleAppender
-log4j.appender.stdout.Target = System.out
-log4j.appender.stdout.layout = org.apache.log4j.PatternLayout
-log4j.appender.stdout.layout.ConversionPattern = [%-5p] %d{yyyy-MM-dd HH:mm:ss,SSS} method:%l%n%m%n
-```
-
-## Maven
-
-`仅限公司内部项目配置生效`
-
-```xml
-<dependency>
-    <groupId>site.dunhanson.email</groupId>
-    <artifactId>easy-email</artifactId>
-    <version>2020.0425.2130</version>
-</dependency>
-```
-
-关联依赖
-```xml
-<dependencies>
-    <dependency>
-        <groupId>junit</groupId>
-        <artifactId>junit</artifactId>
-        <version>4.13</version>
-        <scope>test</scope>
-    </dependency>
-    <dependency>
-        <groupId>org.apache.commons</groupId>
-        <artifactId>commons-email</artifactId>
-        <version>1.5</version>
-    </dependency>
-    <dependency>
-        <groupId>org.projectlombok</groupId>
-        <artifactId>lombok</artifactId>
-        <version>1.18.12</version>
-        <scope>provided</scope>
-    </dependency>
-    <dependency>
-        <groupId>org.slf4j</groupId>
-        <artifactId>slf4j-api</artifactId>
-        <version>1.7.30</version>
-    </dependency>
-    <dependency>
-        <groupId>org.slf4j</groupId>
-        <artifactId>slf4j-log4j12</artifactId>
-        <version>1.7.30</version>
-        <scope>test</scope>
-    </dependency>
-</dependencies>
-```
-
-## 参考资料
-
-Apache Commons-Email
-
-http://commons.apache.org/proper/commons-email/userguide.html
-
